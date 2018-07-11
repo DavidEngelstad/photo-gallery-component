98aa13fea89a637e0a628cb197d512d9dcf016e6
David Engelstad <davidengelstad@gmail.com>
2018-07-10 18:57:37 -0700

    Begin server-side render

--- server/index.js
+++ server/index.js
@@ -17,21 +17,6 @@ require('../db/config/mongo.js');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// profiles.on('route', ({ req, elapsedMS }) => {
-//     console.log('This one...', req.method, req.url, `${elapsedMS}ms`);
-//   });
-  
-//   // Make sure you register this **before** other middleware
-// app.use(function profilerMiddleware(req, res, next) {
-//   const start = Date.now();
-//     // The 'finish' event will emit once the response is done sending
-//   res.once('finish', () => {
-//       // Emit an object that contains the original request and the elapsed time in MS
-//     profiles.emit('route', { req, elapsedMS: Date.now() - start });
-//   });
-//   next();
-//   });
-
 // app.use(helmet());
 // app.use(morgan('dev'));
 // app.use(responseTime());
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,79 @@
+// const path = require('path');
+
+// const SRC_DIR = path.resolve(__dirname, './client');
+// const BUILD_DIR = path.resolve(__dirname, './static');
+
+// module.exports = {
+//   entry: path.resolve(SRC_DIR, 'index.jsx'),
+//   output: {
+//     filename: 'bundle.js',
+//     path: BUILD_DIR
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: [/node_modules/],
+//         use: [{
+//           loader: 'babel-loader',
+//           options: { presets: ['es2015', 'react', 'env'] }
+//         }],
+//       },
+//       {
+//         test: /\.css$/,
+//         loader: 'style-loader'
+//       }, {
+//         test: /\.css$/,
+//         loader: 'css-loader',
+//         query: {
+//           modules: true,
+//           localIdentName: '[name]__[local]___[hash:base64:5]'
+//         }
+//       }
+//     ]
+//   }
+// }
+
+const webpack = require('webpack');
 const path = require('path');
 
-const SRC_DIR = path.resolve(__dirname, './client');
-const BUILD_DIR = path.resolve(__dirname, './static');
+// See: https://stackoverflow.com/questions/37788142/webpack-for-back-end
 
-module.exports = {
-  entry: path.resolve(SRC_DIR, 'index.jsx'),
-  output: {
-    filename: 'bundle.js',
-    path: BUILD_DIR
-  },
+const common = {
+  context: __dirname + '/client',
   module: {
-    rules: [
+    loaders: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: [/node_modules/],
-        use: [{
-          loader: 'babel-loader',
-          options: { presets: ['es2015', 'react', 'env'] }
-        }],
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader'
-      }, {
-        test: /\.css$/,
-        loader: 'css-loader',
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
         query: {
-          modules: true,
-          localIdentName: '[name]__[local]___[hash:base64:5]'
-        }
-      }
-    ]
+          presets: ['react', 'es2015', 'env']
+        },
+      },
+    ],
   }
-}
+};
+
+const client = {
+  entry: './client.js',
+  output: {
+    path: __dirname + '/static',
+    filename: 'app.js'
+  }
+};
+
+const server = {
+  entry: './server.js',
+  target: 'node',
+  output: {
+    path: __dirname + '/static',
+    filename: 'app-server.js',
+    libraryTarget: 'commonjs-module'
+  }
+};
+
+module.exports = [
+  Object.assign({}, common, client),
+  Object.assign({}, common, server)
+];
